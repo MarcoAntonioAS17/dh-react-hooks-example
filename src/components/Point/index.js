ee145935038f5feafece3b046b2cbaebdae10556
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-09-30 20:17:38 -0500

    challenges

--- src/components/Point/index.js
+++ src/components/Point/index.js
@@ -1,9 +1,9 @@
 import "./point.css"
 
-export default ({ coordinates }) => {
+export default ({ coordinates, color, name }) => {
     return (
-        <div className="point" style={{ top: (coordinates.positionY), left: coordinates.positionX }}>
-            x
+        <div className="point" style={{ top: (coordinates.positionY), left: coordinates.positionX ,  background: color}}>
+            {name}
         </div>
     )
 }
--- src/containers/PointerArea/index.js
+++ src/containers/PointerArea/index.js
@@ -1,8 +1,30 @@
 import "./PointerArea.css"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Point from "../../components/Point"
- const PointerArea = () => {
+
+const PointerArea = () => {
     const [clickPosition, setClickPoisiton] = useState({ positionX: 0, positionY: 0})
+    const [data, setData] = useState([]);
+
+    useEffect (()=>{
+        const position = localStorage.getItem('position')
+        let arreglo = position.split(',')
+
+        setClickPoisiton({
+            positionX: parseInt(arreglo[0]),
+            positionY: parseInt(arreglo[1])
+        })
+        console.log(arreglo);
+        console.log(typeof(arreglo))
+
+        fetch("https://611b12ac22020a00175a4323.mockapi.io/api/v1/points")
+        .then( response => response.json()
+        ).then(data => {
+            console.log(data);
+            setData(data)
+        })
+
+    },[]);
 
     const watchClick = (e) => {
         console.log({
@@ -13,10 +35,23 @@ import Point from "../../components/Point"
             positionX: e.clientX - 15,
             positionY: e.clientY - 15
         })
+        localStorage.setItem("position",[e.clientX - 15, e.clientY - 15])
+        
     }
     return (
         <div className="container" onClick={watchClick}>
-            <Point coordinates={clickPosition} />
+            <Point coordinates={clickPosition} color="red" name="X"/>
+
+            
+            {  data.map(item => {
+                return <Point  key={item.id} coordinates={{
+                    positionX: (item.x - 15),
+                    positionY: (item.y - 15)
+                    }}
+                    color = {item.color}
+                    name = {item.id}
+                    />
+            })}
         </div>
     )
 }
